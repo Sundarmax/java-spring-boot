52505c45cf78beef1743cd8a83a96a4df9362622
Sundar <sundar.rajan@symphonyretailai.com>
2025-11-20 11:37:20 +0530

    Add lazy initialization

--- src/main/java/com/sundar/springboot/demo/myfirstapp/common/CricketCoach.java
+++ src/main/java/com/sundar/springboot/demo/myfirstapp/common/CricketCoach.java
@@ -1,11 +1,18 @@
 package com.sundar.springboot.demo.myfirstapp.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 @Primary
 public class CricketCoach implements Coach {
+
+    public CricketCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 10 minutes :)";
